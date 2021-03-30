b8646745f8687288b0c1ddf456f5fa54e27b2846
rockbr <vitor_oliv@hotmail.com>
2021-03-30 08:17:25 -0300

    Config

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'https://adm.brcosmetic.com';
+	public $baseURL = 'https://www.adm.brcosmetic.com';
 
 	/*
 	|--------------------------------------------------------------------------
